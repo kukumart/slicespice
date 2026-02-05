0165fb0c07149a314966a4663e2ab9bf1881f042
Njoro N <info@unihost.co.ke>
2026-02-05 03:42:02 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "process.env.NEXT_PUBLIC_FIREBASE_API_KEY",
-  authDomain: "process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN",
-  projectId: "process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID",
-  storageBucket: "process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "process.env.NEXT_PUBLIC_FIREBASE_APP_ID"
+  "projectId": "studio-2954956221-d79a9",
+  "appId": "1:120951427362:web:7a4d65b5dc5315d4fd157b",
+  "apiKey": "AIzaSyDpUBM4389RvBjY9m98o6jfHxprKsay-tc",
+  "authDomain": "studio-2954956221-d79a9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "120951427362"
 };
